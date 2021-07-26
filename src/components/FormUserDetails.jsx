c27fad34c247930a4442061480c359a923f99554
yogeshwari-jhala <yogeshwarijhala1627@gmail.com>
2021-07-26 23:23:43 +0530

    form completed

--- src/components/FormUserDetails.jsx
+++ src/components/FormUserDetails.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Card } from '@material-ui/core';
+import './Style.css'
+
 
 export class FormUserDetails extends Component {
   continue = e => {
@@ -14,14 +15,11 @@ export class FormUserDetails extends Component {
   render() {
     const { values, handleChange } = this.props;
     return (
-      <MuiThemeProvider>
+      
         <>
-          <Dialog
-            open
-            fullWidth
-            maxWidth='sm'
-          >
-            <AppBar title="Enter User Details" />
+        
+            <AppBar position="static" className="bar">Enter User Details</AppBar>
+            <Card className="card">
             <TextField
               placeholder="Enter Your First Name"
               label="First Name"
@@ -53,10 +51,12 @@ export class FormUserDetails extends Component {
               color="primary"
               variant="contained"
               onClick={this.continue}
+              className="button"
             >Continue</Button>
-          </Dialog>
+            </Card>
+          {/* </Dialog> */}
         </>
-      </MuiThemeProvider>
+    
     );
   }
 }
